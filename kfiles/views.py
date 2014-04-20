3fcd9b15ba73686062ec5f79e08220e4d2345d46
Jacob Sayles <Jacob@Tesla.local>
2014-04-19 17:51:57 -0700

    Now with prettier email password reset

--- kfiles/views.py
+++ kfiles/views.py
@@ -4,6 +4,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import PasswordResetForm
 from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.models import Site
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, Http404, HttpResponseServerError, HttpResponseRedirect, HttpResponsePermanentRedirect
 from django.template import RequestContext, Template
 from forms import UploadFileForm
@@ -13,7 +15,7 @@ from models import File_Upload
 def index(request):
 	return render_to_response('index.html',{}, RequestContext(request))
 
-def password_reset(request, is_admin_site=False, template_name='registration/password_reset_form.html', email_template_name='registration/password_reset_email.html', password_reset_form=PasswordResetForm, token_generator=default_token_generator,post_reset_redirect=None):
+def password_reset(request, is_admin_site=False, template_name='password_reset_form.html', email_template_name='password_reset_email.html', password_reset_form=PasswordResetForm, token_generator=default_token_generator,post_reset_redirect=None):
 	if post_reset_redirect is None: post_reset_redirect = reverse('django.contrib.auth.views.password_reset_done')
 	if request.method == 'GET' and request.GET.get('email',None):
 		form = password_reset_form(initial={'email':request.GET.get('email')})
